a5450c9c3ff54d9656cbf0b7bf99dea261d5dd27
Aman <aman60pant@gmail.com>
2025-08-07 15:24:15 +0530

    update style.css and our-stay.php

--- our-stay.php
+++ our-stay.php
@@ -1,2 +1,15 @@
 <?php include 'include/header.php'; ?>
 
+<div class="row mx-auto mt-5">
+    <div class="col-lg-12 Stay-section">
+        <h1 class="text-center my-5">Our Stay</h1>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+
+        </div>
+    </div>
+</div>
+<?php include 'include/footer.php'; ?>
